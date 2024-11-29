3d56df8a4a335255f1f247884623fed82334b131
Kunalkbhatia <bhatiakbkb@gmail.com>
2024-11-29 21:28:18 +0530

    refactor: renamed app in sidebar

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -9,6 +9,6 @@ import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
  */
 export const baseOptions: BaseLayoutProps = {
   nav: {
-    title: 'My App',
+    title: 'Hook Master 🚀',
   },
 };
