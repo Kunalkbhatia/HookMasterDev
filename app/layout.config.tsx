ae2a8c5f63fa642b8628c398a0befb8deaccef11
Kunalkbhatia <bhatiakbkb@gmail.com>
2024-11-29 21:27:12 +0530

    minor

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -11,11 +11,4 @@ export const baseOptions: BaseLayoutProps = {
   nav: {
     title: 'My App',
   },
-  links: [
-    {
-      text: 'Documentation',
-      url: '/docs',
-      active: 'nested-url',
-    },
-  ],
 };
